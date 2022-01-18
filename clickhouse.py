ee875b82faf67f0ed935d632c73c7e3aaccde89a
Michał Sidor <public+git@meekchopp.es>
2022-01-18 21:58:33 +0100

    oh this one is case-sensitive wow

--- clickhouse.py
+++ clickhouse.py
@@ -5,7 +5,7 @@ import itertools
 import uuid
 uuids = sorted(uuid.UUID(bytes=bytes(x)) for x in itertools.product((0, 1), repeat=16))
 conn = Client("localhost")
-conn.execute("CREATE TABLE a (u uuid) ENGINE = MergeTree() ORDER BY u")
+conn.execute("CREATE TABLE a (u UUID) ENGINE = MergeTree() ORDER BY u")
 conn.execute("INSERT INTO a (u) VALUES", [{"u": u} for u in uuids])
 for row, u in zip(conn.execute("SELECT u FROM a ORDER BY u"), uuids):
     print(row[0], u)

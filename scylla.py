b808ab467281bb8723f92c034c408ee18301aab6
Michał Sidor <public+git@meekchopp.es>
2022-01-18 20:51:49 +0100

    fix: i guess you can't skip this

--- scylla.py
+++ scylla.py
@@ -6,11 +6,10 @@ import uuid
 uuids = sorted(uuid.UUID(bytes=bytes(x)) for x in itertools.product((0, 1), repeat=16))
 cluster = Cluster()
 conn = cluster.connect()
-conn.execute("CREATE KEYSPACE main")
-conn.execute("USE main")
-conn.execute("CREATE TABLE a (u uuid PRIMARY KEY)")
+conn.execute("CREATE KEYSPACE main WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}")
+conn.execute("CREATE TABLE main.a (u uuid PRIMARY KEY)")
 for u in uuids:
-    conn.execute("INSERT INTO a (u) VALUES (%s)", (u,))
-for row, u in zip(conn.execute("SELECT u FROM a ORDER BY u"), uuids):
+    conn.execute("INSERT INTO main.a (u) VALUES (%s)", (u,))
+for row, u in zip(conn.execute("SELECT u FROM main.a ORDER BY u"), uuids):
     print(row[0], u)
     assert row[0] == u
